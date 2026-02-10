a56e96cea5922b66ef0e9e7abb3b134342c1b28c
Jakob Goebel <goebel.jakob@gmail.com>
2026-02-10 13:13:27 +0100

    Just a whole bunch of linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import js from '@eslint/js';
 import globals from 'globals';
 import json from '@eslint/json';
 import markdown from '@eslint/markdown';
-import css from '@eslint/css';
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
@@ -55,20 +54,15 @@ export default defineConfig([
             ],
         },
     },
-    { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+    {
+        files: ['**/*.json'],
+        plugins: { json },
+        language: 'json/json',
+        extends: ['json/recommended'],
+        ignores: ['package-lock.json'],
+    },
     { files: ['**/*.jsonc'], plugins: { json }, language: 'json/jsonc', extends: ['json/recommended'] },
     { files: ['**/*.json5'], plugins: { json }, language: 'json/json5', extends: ['json/recommended'] },
     { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: ['markdown/recommended'] },
-    {
-        files: ['**/*.css'],
-        plugins: { css },
-        language: 'css/css',
-        extends: ['css/recommended'],
-        rules: {
-            'css/no-important': ['off'],
-            'css/use-baseline': ['off'],
-            'css/no-invalid-properties': ['off'],
-        },
-    },
     eslintConfigPrettier,
 ]);
---
+++ stylelint.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: ['stylelint-config-standard-scss'],
+    plugins: ['stylelint-scss'],
+    rules: {
+        'selector-class-pattern': null,
+        'selector-id-pattern': null,
+        'scss/at-mixin-pattern': null,
+        'scss/dollar-variable-pattern': null,
+    },
+};
